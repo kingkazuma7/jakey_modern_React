bb86897d0c10785c5336aa03b8d699a271f5834d
kingkazuma7 <your.nasionasio7@gmail.com>
2024-09-14 19:36:34 +0900

    inputから値を入力して未完了エリアにli生成、挿入まで

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,35 @@
-console.log("omiko");
+// 追加をクリックしたとき
+// li>div>p+button+buttonを生成する
+// 完了をクリックしたとき
+// 削除をクリックしたとき
+// 戻すをクリックしたとき
+// inputの入力を取得する
+
+const inputTodo = document.getElementById("input-todo");
+const buttonAdd = document.getElementById("button-add");
+const incompleteTodo = document.getElementById("incomplete-todo");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const onClickAdd = () => {
+    const inputValue = inputTodo.value;
+    inputTodo.value = ""; // 初期化
+
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+          div.className = "list-row";
+    const p = document.createElement("p");
+          p.innerText = inputValue;
+    const buttonComplete = document.createElement("button");
+          buttonComplete.innerText = "完了";
+    const buttonDelete = document.createElement("button");
+          buttonDelete.innerText = "削除";
+
+    div.appendChild(p);
+    div.appendChild(buttonComplete);
+    div.appendChild(buttonDelete);
+    li.appendChild(div);
+    incompleteTodo.appendChild(li);
+  };
+
+  buttonAdd.addEventListener("click", onClickAdd);
+});
