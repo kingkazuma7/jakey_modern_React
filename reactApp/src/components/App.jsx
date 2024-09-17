cc9a945a1e69cd1569ff25479254f8169b412b27
kingkazuma7 <your.nasionasio7@gmail.com>
2024-09-17 20:11:39 +0900

    48. useState 状態変数と更新用関数 ※分割代入

--- reactApp/src/components/App.jsx
+++ reactApp/src/components/App.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import { ColorfulMessage } from "./ColorfulMessage";
 
 export const App = () => {
-  const onClickButton = () => alert();
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    setNum(num+1);
+  }
 
   return (
     <>
       <h1>こんにちは!</h1>
       <ColorfulMessage color="red" message="お元気ですか？"  />
       <ColorfulMessage color="green">お元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   )
 }
