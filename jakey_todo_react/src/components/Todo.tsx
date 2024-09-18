20c49e55c6871ba58478f185ce53082b99535b1c
kingkazuma7 <your.nasionasio7@gmail.com>
2024-09-18 12:42:45 +0900

    55 ベタでマークアップ

--- jakey_todo_react/src/components/Todo.tsx
+++ jakey_todo_react/src/components/Todo.tsx
@@ -1,8 +1,49 @@
 import React from 'react'
 
-const Todo = () => {
+export const Todo = () => {
   return (
-    <div>Todo</div>
+    <React.Fragment>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className='title'>未完了のTODO</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p className='todo-item'>TODOです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li>
+            <div className="list-row">
+              <p className='todo-item'>TODOです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className='complete-area'>
+        <p className='title'>完了のTODO</p>
+        <ul>
+          <li>
+            <div className="list-row">
+            <p className='todo-item'>TODOでした</p>
+            <button>戻す</button>
+            </div>
+          </li>
+          <li>
+              <div className="list-row">
+              <p className='todo-item'>TODOでした</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
   )
 }
 
--- jakey_todo_react/src/index.tsx
+++ jakey_todo_react/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Todo from './components/Todo';
+import './style.css';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
