a35b70e2bd41d5a65b10d926a2545f141ac5939f
kingkazuma7 <your.nasionasio7@gmail.com>
2024-09-21 12:48:30 +0900

    インラインスタイル inputTodo

--- jakey_todo_react/src/components/inputTodo.tsx
+++ jakey_todo_react/src/components/inputTodo.tsx
@@ -5,11 +5,20 @@ export interface InputTodoProps {
   placeholder?: string;
 }
 
+const style = {
+    backgroundColor: '#c6e5d9',
+    width: '400px',
+    height: '30px',
+    padding: '8px',
+    margin: '8px',
+    borderRadius: '8px'
+}
+
 export const InputTodo = (props: InputTodoProps) => {
   const { todoText, handleInputChange, addTask, placeholder } = props;
 
   return (
-    <div className="input-area">
+    <div style={style}>
       <input placeholder={placeholder} value={todoText} onChange={handleInputChange} />
       <button onClick={addTask}>追加</button>
     </div>
